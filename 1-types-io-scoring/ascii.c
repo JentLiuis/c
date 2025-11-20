7a27159d0716c34e4e73131c9f03385074f4109d
jent <1985616987@qq.com>
2025-11-21 00:01:22 +0800

    push-test

---
+++ 1-types-io-scoring/ascii.c
@@ -0,0 +1,14 @@
+//
+// Created by jent on 24-10-11.
+//
+#include <stdio.h>
+    int main(){
+
+        int x ;
+
+        scanf("%d",&x);
+
+        printf("%d %c",x,x);
+
+        return 0;
+    }
